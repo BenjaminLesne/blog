5ac9ea89b403f63a9f8efb7487cbb00a94952ab2
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-01-08 17:10:21 +0100

    updated metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: '',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://blog.benjamin-lesne.dev',
+  siteRepo: 'https://github.com/BenjaminLesne/blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
