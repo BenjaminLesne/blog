94715a4c286b3c9ea9e82d3ec2deae379a88103f
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-12-29 17:04:51 +0100

    wip: doc: custom bluesky feed

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -6,3 +6,15 @@ const headerNavLinks = [
 ]
 
 export default headerNavLinks
+
+enum Gift {
+  Coal = 0,
+  Train = 1,
+  Bicycle = Train << 1,
+  SuccessorToTheNintendoSwitch = Bicycle << 1,
+  TikTokPremium = SuccessorToTheNintendoSwitch << 1,
+  Vape = TikTokPremium << 1,
+  Traditional = Train | Bicycle,
+  OnTheMove = Coal | TikTokPremium | Vape | Bicycle,
+  OnTheCouch = (OnTheMove & ~Bicycle) | SuccessorToTheNintendoSwitch,
+}
